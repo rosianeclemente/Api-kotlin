93857e0ef8e09176241675c978b05af61a56321d
Rosiane Clemente <rosianeclemente1997@gmail.com>
2021-12-27 21:13:13 -0300

    first commit

---
+++ ApiKotlin/src/main/kotlin/com/example/apiKotlin/ApiKotlin/ApiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apiKotlin.ApiKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiKotlinApplication>(*args)
+}
---
+++ ApiKotlin/src/test/kotlin/com/example/apiKotlin/ApiKotlin/ApiKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apiKotlin.ApiKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
